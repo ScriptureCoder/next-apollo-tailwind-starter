a5865b5b96acf528267e80fb503257e71b2ebee2
ScriptureCoder <akinfenwasamson@gmail.com>
2021-11-06 17:48:19 +0100

    update

--- apollo-client.ts
+++ apollo-client.ts
@@ -1,9 +1,10 @@
 import {HttpLink} from "apollo-link-http";
 import {WebSocketLink} from "apollo-link-ws";
-import {split} from "apollo-link";
+import {ApolloLink, split} from "apollo-link";
 import {getMainDefinition} from "apollo-utilities";
 import {InMemoryCache} from "apollo-cache-inmemory";
 import ApolloClient from "apollo-client";
+import {browser} from "./utils/constants";
 
 let Authorization = null;
 const token = "store.getState().auth";
@@ -19,30 +20,34 @@ const httpLink = new HttpLink({
     }
 });
 
-const wsLink = typeof window === 'undefined'?{}:new WebSocketLink({
-    uri: process.env.NEXT_PUBLIC_WS_URL,
-    options: {
-        reconnect: true,
-        connectionParams: {
-            headers:{
-                authorization:Authorization
-            }
+let link: ApolloLink = httpLink;
+if (browser){
+    const wsLink = new WebSocketLink({
+        uri: process.env.NEXT_PUBLIC_WS_URL||"",
+        options: {
+            reconnect: true,
+            connectionParams: {
+                headers:{
+                    authorization:Authorization
+                }
+            },
+
+        }
+    });
+
+    link = split(
+        ({ query }) => {
+            const definition = getMainDefinition(query);
+            return (
+                definition.kind === 'OperationDefinition' &&
+                definition.operation === 'subscription'
+            );
         },
+        wsLink,
+        httpLink,
+    );
+}
 
-    }
-});
-
-const link = typeof window === 'undefined'?{}:split(
-    ({ query }) => {
-        const definition = getMainDefinition(query);
-        return (
-            definition.kind === 'OperationDefinition' &&
-            definition.operation === 'subscription'
-        );
-    },
-    wsLink,
-    httpLink,
-);
 
 export const cache = new InMemoryCache();
 export const client = new ApolloClient({
